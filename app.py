4ce9acea17a01633d8c58c87798d0988b7a93eb4
Sindhu N <sindhuureddy03@gmail.com>
2025-03-16 13:56:34 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import ee
+
+# Authenticate with Google Earth Engine
+service_account = "sindhu@imperial-data-453906-t8.iam.gserviceaccount.com"
+key_path = "gee_key.json"  # Path to your JSON key file
+
+credentials = ee.ServiceAccountCredentials(service_account, key_path)
+ee.Initialize(credentials)
+
+st.title("Satellite Image Segmentation")
+st.write("Google Earth Engine Initialized Successfully!")
